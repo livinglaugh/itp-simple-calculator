3fdeaa7e45202ba399d622043481b664686f19f1
livinglaugh <92323075+livinglaugh@users.noreply.github.com>
2021-10-11 11:04:47 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,54 +1,23 @@
 def add(x, y):
-    if type(x) != int or type(y) != int:
-        return None
-    else:
-        return x + y
-
+    return x + y
 
 def subtract(x, y):
-    if type(x) != int or type(y) != int:
-        return None
-    else:
-        return x - y
-
+    return x - y
 
 def divide(x, y):
-    if type(x) != int or type(y) != int:
-        return None
-    elif y == 0:
+    if y == 0:
         return "Invalid value for denominator, can't divide by 0!"
     else:
         return x / y
     
-
-
 def multiply(x, y):
-    if type(x) != int or type(y) != int:
-        return None
-    else:
-        return x * y
+    return x * y
     
-
-
 def square(x):
-    if type(x) != int:
-        return None
-    else:
-        return x ** 2
+    return x ** 2
     
-
-
 def power(x, y):
-    if type(x) != int or type(y) != int:
-        return None
-    else:
-        return x ** y
+    return x ** y
     
-
-
 def sqrt(x):
-    if type(x) != int:
-        return None
-    else:
-        return x ** (1/2)
-    
+    return power(x, 1/2)
